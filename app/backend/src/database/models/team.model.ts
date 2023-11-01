630f47bea348cd6a0c282271197657be5d79d3f0
Luis Correia <pleluis47@gmail.com>
2023-11-01 19:20:07 -0300

    feat req 1

---
+++ app/backend/src/database/models/team.model.ts
@@ -0,0 +1,30 @@
+import { DataTypes, Model, ModelDefined, Optional } from "sequelize";
+import Team from '../../Interfaces/Team';
+import db from '.';
+
+type TeamInputtableTypes = Optional<Team, 'id'>;
+type TeamSequilizeModelCreator = ModelDefined<Team, TeamInputtableTypes>;
+export type TeamModel = Model<Team, TeamInputtableTypes>;
+
+const TeamModelCreator: TeamSequilizeModelCreator = db.define('team', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+export default TeamModelCreator;
