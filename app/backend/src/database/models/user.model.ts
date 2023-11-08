4e62114b38bb1a1792f1a104c8df9a3ba61794dd
Luis Correia <pleluis47@gmail.com>
2023-11-08 15:56:20 -0300

    feat: add user model&migration

---
+++ app/backend/src/database/models/user.model.ts
@@ -0,0 +1,37 @@
+import { DataTypes, Model, Optional, ModelDefined } from 'sequelize';
+import IUser from '../../Interfaces/IUser';
+import db from '.';
+
+type UserModelSequelizeAttributes = Optional<IUser, 'id'>;
+type UserModelCreator = ModelDefined<IUser, UserModelSequelizeAttributes>;
+export type IUserModel = Model<IUser, UserModelSequelizeAttributes>;
+const UserModelSequelize: UserModelCreator = db.define('Team', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'users',
+  underscored: true,
+  timestamps: false,
+});
+
+export default UserModelSequelize;
